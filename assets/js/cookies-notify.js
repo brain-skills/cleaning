a4a08c9fbe161664f4a237eba2e8445b30879f27
brain-skills <web.pr@mail.ru>
2023-07-04 19:58:07 +0400

    init version 0.1.1.0

--- assets/js/cookies-notify.js
+++ assets/js/cookies-notify.js
@@ -1,10 +1,10 @@
-let clearLs = document.querySelector('#clearLs');
-let cookiesNotify = document.querySelector('#cookiesNotify');
-let notifysubmit = document.querySelector('#notifysubmit');
-clearLs.addEventListener('click', (e)=>{
-    localStorage.clear();
-    e.preventDefault();
-});
+// let clearLs = document.querySelector('#clearLs');
+// let cookiesNotify = document.querySelector('#cookiesNotify');
+// let notifysubmit = document.querySelector('#notifysubmit');
+// clearLs.addEventListener('click', (e)=>{
+//     localStorage.clear();
+//     e.preventDefault();
+// });
 setTimeout(() =>{
     if (localStorage.getItem('cookies') == null) {
         cookiesNotify.classList.add('show');
