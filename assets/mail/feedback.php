d3310121f96685f82279c0ce90e0d10683ff560d
brain-skills <web.pr@mail.ru>
2024-03-29 08:13:45 +0400

    init version 0.1.7.2

--- assets/mail/feedback.php
+++ assets/mail/feedback.php
@@ -12,15 +12,8 @@ $headers .= "Reply-To: $to" . PHP_EOL;
 
 $headers .= "MIME-Version: 1.0" . PHP_EOL;
 $headers .= "Content-type: text/html; charset=utf-8" . PHP_EOL;
-$headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
 
 $message = "<p><strong>Имя:</strong> $yourname</p>
 \n<p><strong>Телефон:</strong> $yourphone</p>";
 
-if (mail($to, $subject, $message, $headers)) {
-    echo "Письмо успешно отправлено!";
-} else {
-    echo "Ошибка при отправке письма.";
-    // Здесь можно добавить дополнительные действия, такие как запись в лог или отправка сообщения об ошибке администратору.
-    error_log("Ошибка при отправке письма на адрес: $to", 0);
-}
+mail($to, $subject, $message, $headers);
